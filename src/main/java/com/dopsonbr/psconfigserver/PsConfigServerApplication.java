0227b2f0899bee26135c8ef7a1c5a0d58e25cf8b
Brian Dopson <dopsonbr@gmail.com>
2017-11-18 21:25:30 -0500

    add eureka

--- src/main/java/com/dopsonbr/psconfigserver/PsConfigServerApplication.java
+++ src/main/java/com/dopsonbr/psconfigserver/PsConfigServerApplication.java
@@ -1,8 +1,18 @@
 package com.dopsonbr.psconfigserver;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableAutoConfiguration
+@EnableConfigServer
+@EnableDiscoveryClient
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 @SpringBootApplication
 public class PsConfigServerApplication {
 
