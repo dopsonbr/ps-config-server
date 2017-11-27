4df348d10f13d6617816b5d611e46debae05c256
Brian Dopson <dopsonbr@gmail.com>
2017-11-27 13:22:43 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -12,4 +12,6 @@ echo "********************************************************"
 
 java -agentlib:jdwp=transport=dt_socket,address=5006,server=y,suspend=n \
 	-Djava.security.egd=file:/dev/./urandom \
-	-jar /work/*.jar 
+	-Deureka.client.serviceUrl.defaultZone=$EUREKA_SERVER_URI \
+	-Dspring.cloud.config.uri=$CONFIG_SERVER_URI \
+	-jar /work/*.jar
